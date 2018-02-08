79065f2e65aa4b2de6ebc4306720c08c67e95594
MaksUr <maksim.urakv@gmail.com>
2018-02-08 13:54:05 +0500

    add models and constants

--- flights/constants.py
+++ flights/constants.py
@@ -1,7 +1,50 @@
-# class Flight
 
+# model City
+CITY_VERBOSE_NAME = 'Город'
+CITY_VERBOSE_NAME_PLURAL = 'Города'
+# name
+CITY_NAME = 'name'
+CITY_NAME_KEY = 'Название города'
+CITY_NAME_HELP = ''
+# code
+CITY_CODE = 'code'
+CITY_CODE_KEY = 'Код города'
+CITY_CODE_HELP = 'Должен соответсвовать коду IATA. Состоит из 3 букв.'
+# timezone
+CITY_TIMEZONE = 'timezone'
+CITY_TIMEZONE_KEY = 'Часовой пояс'
+CITY_TIMEZONE_HELP = ''
+
+# model Airport
+AIRPORT_VERBOSE_NAME = 'Aэропорт'
+AIRPORT_VERBOSE_NAME_PLURAL = 'Aэропорты'
+# name
+AIRPORT_NAME = 'name'
+AIRPORT_NAME_KEY = 'Название аэропорта'
+AIRPORT_NAME_HELP = ''
+# code
+AIRPORT_CODE = 'code'
+AIRPORT_CODE_KEY = 'Код аэропорта'
+AIRPORT_CODE_HELP = 'Должен соответсвовать коду IATA. Состоит из 3 букв.'
+
+# model Airline
+AIRLINE_VERBOSE_NAME = 'Авиалиния'
+AIRLINE_VERBOSE_NAME_PLURAL = 'Авиалинии'
+# name
+AIRLINE_NAME = 'name'
+AIRLINE_NAME_KEY = 'Авиакомпания'
+AIRLINE_NAME_HELP = ''
+# code
+AIRLINE_CODE = 'code'
+AIRLINE_CODE_KEY = 'Код авиакомпании'
+AIRLINE_CODE_HELP = 'Должен соответсвовать коду IATA. Состоит из 3 букв.'
+
+# model Flight
+FLIGHT_VERBOSE_NAME = 'Рейс'
+FLIGHT_VERBOSE_NAME_PLURAL = 'Рейсы'
 # number
+FLIGHT_NUMBER = 'number'
 FLIGHT_NUMBER_KEY = 'Номер рейса'
-
+FLIGHT_NUMBER_HELP = 'Состоит из цифр, не более четырех.'
 
 
--- flights/models.py
+++ flights/models.py
@@ -1,10 +1,44 @@
 from django.db import models
 
 # Create your models here.
-from django.db.models import CharField
+from django.db.models import CharField, IntegerField, ForeignKey
 
-from flights.constants import FLIGHT_NUMBER_KEY
+from flights.constants import FLIGHT_NUMBER_KEY, AIRLINE_NAME_KEY, AIRLINE_CODE_KEY, \
+    AIRLINE_VERBOSE_NAME, AIRLINE_VERBOSE_NAME_PLURAL, FLIGHT_VERBOSE_NAME, FLIGHT_VERBOSE_NAME_PLURAL, \
+    CITY_VERBOSE_NAME, CITY_VERBOSE_NAME_PLURAL, CITY_NAME_KEY, CITY_CODE_KEY, AIRPORT_VERBOSE_NAME, \
+    AIRPORT_VERBOSE_NAME_PLURAL, AIRPORT_NAME_KEY, AIRPORT_CODE_KEY
+from flights.validators import flight_number_validate
+
+
+class City(models.Model):
+    class Meta:
+        verbose_name = CITY_VERBOSE_NAME
+        verbose_name_plural = CITY_VERBOSE_NAME_PLURAL
+    name = CharField(CITY_NAME_KEY, max_length=60)
+    code = CharField(CITY_CODE_KEY, max_length=3)  # IATA 3-letter city code
+    # timezone = TimeZoneField(CITY_TIMEZONE_KEY)  TODO: add timezone
+
+
+class Airport(models.Model):
+    class Meta:
+        verbose_name = AIRPORT_VERBOSE_NAME
+        verbose_name_plural = AIRPORT_VERBOSE_NAME_PLURAL
+    name = CharField(AIRPORT_NAME_KEY, max_length=60)
+    code = CharField(AIRPORT_CODE_KEY, max_length=3)  # IATA 3-letter airport code
+    city = ForeignKey(City, verbose_name=City._meta.verbose_name)
+
+
+class Airline(models.Model):
+    class Meta:
+        verbose_name = AIRLINE_VERBOSE_NAME
+        verbose_name_plural = AIRLINE_VERBOSE_NAME_PLURAL
+    name = CharField(AIRLINE_NAME_KEY, max_length=60)
+    code = CharField(AIRLINE_CODE_KEY, max_length=2)  # IATA 2-letter code
 
 
 class Flight(models.Model):
-    number = CharField(FLIGHT_NUMBER_KEY, max_length=8, validators=[])
+    class Meta:
+        verbose_name = FLIGHT_VERBOSE_NAME
+        verbose_name_plural = FLIGHT_VERBOSE_NAME_PLURAL
+    number = IntegerField(FLIGHT_NUMBER_KEY, validators=[flight_number_validate])
+    airline = ForeignKey(Airline, verbose_name=Airline._meta.verbose_name)
