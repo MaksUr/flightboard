adbaa9b659e195928fb4b0abb0dc75bc3dbe3761
MaksUr <maksim.urakv@gmail.com>
2018-02-15 19:47:20 +0500

    add order by flight column

--- flights/tables.py
+++ flights/tables.py
@@ -13,7 +13,6 @@ class ScheduleFlightTable(tables.Table):
     duration = tables.Column(accessor='flight.duration')
     departure = AirportColumn(accessor='flight.departure')
     arrival = AirportColumn(accessor='flight.arrival')
-    flight = tables.Column(accessor='flight.codename')
 
     class Meta:
         model = ScheduleFlight
@@ -25,6 +24,16 @@ class ScheduleFlightTable(tables.Table):
         else:
             return 'В пути'
 
+    def render_flight(self, value):
+        return value.codename()
+
     def order_arrival_status(self, queryset, is_descending):
-        queryset = queryset.annotate().order_by(('-' if is_descending else '') + 'time_of_arrival')
+        queryset = queryset.order_by(('-' if is_descending else '') + 'time_of_arrival')
+        return queryset, True
+
+    def order_flight(self, queryset, is_descending):
+        queryset = queryset.order_by(
+            ('-' if is_descending else '') + 'flight__airline__code',
+            ('-' if is_descending else '') + 'flight__number'
+        )
         return queryset, True
